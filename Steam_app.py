1c1982b9dfea6792d58434f089a3bb3e92e8e154
Juliana Figueira <95477212+Julianabsf@users.noreply.github.com>
2021-12-09 15:08:15 +0100

    Update Steam_app.py

--- Steam_app.py
+++ Steam_app.py
@@ -11,42 +11,10 @@ import seaborn as sns
 import warnings
 warnings.filterwarnings('ignore')
 
-
-# In[ ]:
-
-
-#--------------Load the datasets-----------------------
-# Principal dataset:
-steam_data = pd.read_csv("/home/juliana/Documents/DS_Allwomen/_Final_project_dataset/steam_df_clean.csv")
-# Genreal infomation dataset:
-steam_info = pd.read_csv('/home/juliana/Documents/DS_Allwomen/_Final_project_dataset/steam_general.csv')
-# Split platforms dataset:
-# Split genre dataset:
-# Split categories dataset:
-# Split languages dataset:
-
-#--------------Creat the page name----------------------
-
-st.set_page_config(page_title="Steam Analysis- Dashboard", layout='wide')
-
-#---------------Set the oppen menssage -----------------
-
-st.markdown("***'How Steam Games...?'***")
-st.markdown("This app alows you to explore the steam dataset and get games recommendations based on the games you liked.")
-
-
-# In[ ]:
-
-
-#------------------Second Block-------------------------
-
-# Input
-st.markdown("### **What are the informations about your favorite game?**")
-
-select_name = st.text_input("Write the name of your favorite Steam Game: ")
-select_name_ = steam_info[team_info['Game Name']  == select_name]
-
-st.subheader("That's your favorite game informations")
-
-st.table(select_name_)
-
+##############################################################################################
+# PAGE STYLING
+##############################################################################################
+st.set_page_config(page_title="Steam Analysis Dashboard ", 
+                   layout='wide')
+                   
+st.title("Steam Analysis")
