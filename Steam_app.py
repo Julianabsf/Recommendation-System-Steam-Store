b1ca959bbafb0226284ecc04310d7003166df137
Juliana Figueira <95477212+Julianabsf@users.noreply.github.com>
2021-12-10 17:59:29 +0100

    Update Steam_app.py

--- Steam_app.py
+++ Steam_app.py
@@ -419,11 +419,12 @@ st.table(genre_recommendations)
     text = """\
     - Name: 'Ivy Wang'
 - Feature: 'Always make cool stuff'
-            - Occupation: 'Data Scientist'
+- Occupation: 'Data Scientist'
     
-                - [**Linkedin**](https://www.linkedin.com/in/ivy-w-81871716b/),
-                    - [**twitter**](twitter.com/Ivy_DataCracker),
-    **Thanks for your viewing!**
+- [**Linkedin**](https://www.linkedin.com/in/ivy-w-81871716b/),
+- [**twitter**](twitter.com/Ivy_DataCracker),
+
+**Thanks for your viewing!**
 """
     st.sidebar.markdown(text)
  
