8816d58cd17cac7095ffc38e82bd93bcc1bf9b4a
Juliana Figueira <95477212+Julianabsf@users.noreply.github.com>
2021-12-10 17:57:23 +0100

    Update Steam_app.py

--- Steam_app.py
+++ Steam_app.py
@@ -417,9 +417,9 @@ st.table(genre_recommendations)
 
     st.sidebar.subheader("About Author")
     text = """\
-    - Name: 'Ivy Wang'
+    - Name: 'Ivy Wang',
     - Feature: 'Always make cool stuff',
-    - Occupation: Data Scientist,
+    - Occupation: 'Data Scientist',
     
     - [**Linkedin**](https://www.linkedin.com/in/ivy-w-81871716b/),
     - [**twitter**](twitter.com/Ivy_DataCracker),
