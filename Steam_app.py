da1d09abe6d58dc3cbc0f7debe748c11e3b16f00
Juliana Figueira <95477212+Julianabsf@users.noreply.github.com>
2021-12-10 17:27:54 +0100

    Update Steam_app.py

--- Steam_app.py
+++ Steam_app.py
@@ -27,6 +27,15 @@ from sklearn.neighbors import NearestNeighbors
 import warnings
 warnings.filterwarnings('ignore')
 
+
+#########################
+
+primaryColor="#d33682"
+backgroundColor="#002b36"
+secondaryBackgroundColor="#586e75"
+textColor="#fafafa"
+font="sans serif"
+
 #############################
 # Plots Functions
 #############################
