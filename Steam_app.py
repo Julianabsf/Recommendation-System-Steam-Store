0224836e7c21d5e742b32c358ab4adfb5b3351f3
Juliana Figueira <95477212+Julianabsf@users.noreply.github.com>
2021-12-13 13:58:52 +0100

    Update Steam_app.py

--- Steam_app.py
+++ Steam_app.py
@@ -31,6 +31,12 @@ warnings.filterwarnings('ignore')
 
 #########################
 
+primaryColor="#1b2838"
+backgroundColor="#FFFFFF"
+secondaryBackgroundColor="#F0F2F6"
+textColor="#262730"
+font="sans serif"
+
 #############################
 # Plots Functions
 #############################
